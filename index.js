a4382cc5bb33c119bd80bd257500e1a54c96c393
Bruno Gonçalves <brunogcp28@gmail.com>
2024-05-06 07:56:54 -0400

    Aula de JavaScript básico Condicionais

---
+++ index.js
@@ -0,0 +1,13 @@
+ let nomeFilme = 'Vingadores'
+
+// if(nomeFilme === 'Vingadores' || nomeFilme === 'vingadores'){
+//     console.log("É o filme dos Vingadores")
+// }else if(nomeFilme === 'Liga da justiça'){
+//     console.log("É o filme da Liga da justiça")
+// }else{
+//     console.log("Este filme não existe")
+// }
+
+// true ? console.log("Verdadeira") : console.log("Falso")
+
+nomeFilme === 'Vingadores' ? console.log("É o filme dos Vingadores") : console.log("É outro filme") 
